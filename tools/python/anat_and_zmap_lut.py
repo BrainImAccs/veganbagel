- Fix JPEG quality for export - Fix image rotation (use transpose) - Fix legend creating and colour mapping (especially the thresholds) - Fix description/setting for transparency - Change default colour map - Add reference DICOM to colourLUT call

tools/python/anat_and_zmap_lut.py
@@ -37,12 +37,12 @@ def arg_parser():
                         help='Minimum standard deviations to consider in the LUT (everything below will be transparent)')
     parser.add_argument('--zmax', type=float,  default=10,
                         help='Maximum standard deviations to consider in the LUT (everything above will just stay at the same colour)')
-    parser.add_argument('--cool', type=str, default="cet_linear_blue_5_95_c73_r",
-                        help='Colormap for negative values (default: cet_linear_blue_5_95_c73_r, https://colorcet.holoviz.org/user_guide/index.html#Complete-list)')
+    parser.add_argument('--cool', type=str, default="cet_linear_blue_5_95_c73",
+                        help='Colormap for negative values (default: cet_linear_blue_5_95_c73, https://colorcet.holoviz.org/user_guide/index.html#Complete-list)')
     parser.add_argument('--hot', type=str, default="cet_linear_kryw_0_100_c71",
                         help='Colormap for positive values (default: cet_linear_kryw_0-100_c71, https://colorcet.holoviz.org/user_guide/index.html#Complete-list)')
-    parser.add_argument('--transparency', type=int, default=90, choices=range(0,101), metavar="[0-100]",
-                        help='Transparency in percent (default: 90)')
+    parser.add_argument('--transparency', type=int, default=25, choices=range(0,101), metavar="[0-100]",
+                        help='Transparency of colour map in percent (default: 25)')
     parser.add_argument('--anat-window-center', dest='wcenter', type=int,
                         help='Window center for anatomical images (optional, otherwise center of min/max will be used. Must be defined with --anat-window-width.')
     parser.add_argument('--anat-window-width', dest='wwidth', type=int,
@@ -60,11 +60,11 @@ def main():
         anat = nib.load(args.anat_file).get_fdata().astype(np.int16)
         zmap = nib.load(args.zmap_file).get_fdata().astype(np.float32)
 
-        # Define the colormaps to be used from matplotlib
+        # Define the colour map to be used from matplotlib
         # https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
         hot = plt.get_cmap(args.hot)
         cool = plt.get_cmap(args.cool)
-        # In order to plot the anatomical slices with windowing, we need to apply the reverse Greys colormap
+        # In order to plot the anatomical slices with windowing, we need to apply the reverse Greys "colour" map
         grey = plt.get_cmap("Greys_r")
 
         # Window according to center and width, if supplied
@@ -84,14 +84,14 @@ def main():
         # Ignore some negative values (derived from --zmin) in order to show "normal" z-scores as transparent 
         neg[zmap > (args.zmin * -1)] = float('nan')
         # Turn all remaining values positive and scale by --zmax
-        neg = (neg * -1) / (args.zmax - args.zmin)
+        neg = ((neg * -1) - args.zmin) / (args.zmax - args.zmin)
         # Aynthing greater than --zmax will have the same colour
         neg[neg > 1] = 1
 
         # Same approach as for negative values, but, well keeping it positive ;)
         pos = np.copy(zmap)
         pos[zmap < args.zmin] = float('nan')
-        pos = pos / (args.zmax - args.zmin)
+        pos = (pos - args.zmin) / (args.zmax - args.zmin)
         pos[pos > 1] = 1
 
         # Create an "alpha channel" by combining the pos and neg array
@@ -100,11 +100,11 @@ def main():
         # Anything greater than 0 is therefore of interest, and will be shown
         # Transparency ranges from 0 (complete) to 255 (solid).
         # Keep some transparency for the colour map by multiplying 255 with a configurable value
-        alpha[alpha > 0] = 255 * (args.transparency / 100)
+        alpha[alpha > 0] = 255 * (1 - (args.transparency / 100))
 
         legend_hot = hot(np.linspace([1] * 40, [0] * 40, 75))
         legend_transparent = hot(np.full((75, 40), 0))
-        legend_cool = cool(np.linspace([1] * 40, [0] * 40, 75))
+        legend_cool = cool(np.linspace([0] * 40, [1] * 40, 75))
 
         fnt = ImageFont.truetype('/usr/share/texmf/fonts/opentype/public/tex-gyre/texgyreheroscn-regular.otf', 14)
         L = Image.fromarray((np.vstack((legend_hot, legend_transparent, legend_cool)) * 255).astype(np.uint8))
@@ -123,12 +123,12 @@ def main():
         fnt = ImageFont.truetype('/usr/share/texmf/fonts/opentype/public/tex-gyre/texgyreheroscn-regular.otf', 6)
 
         for i in range(1, anat.shape[2]+1):
-            # Read, min/max scale and colormap the anatomical slice
+            # Read, min/max scale and colour map the anatomical slice
             anat_slice = grey(anat[:,:,i-1])
             sm = cm.ScalarMappable(cmap = grey)
             A = Image.fromarray(sm.to_rgba(anat[:,:,i-1], bytes=True))
 
-            # Read and colormap the positive and negative values into a combined slice, create a RGBA-image
+            # Read and colour map the positive and negative values into a combined slice, create a RGBA-image
             cmap_slice = hot(pos[:,:,i-1]) + cool(neg[:,:,i-1])
             C = Image.fromarray((cmap_slice[:, :, :3] * 255).astype(np.uint8))
 
@@ -136,10 +136,10 @@ def main():
             T = Image.fromarray(alpha[:,:,i-1].astype(np.uint8))
             C.putalpha(T)
 
-            # Overlay the transparent colormap onto the anatomical images
+            # Overlay the transparent colour map onto the anatomical images
             A.paste(C, (0, 0), C)
             # Rotate and mirror for radiological orientation
-            A = ImageOps.mirror(A.rotate(90))
+            A = ImageOps.mirror(A.transpose(Image.ROTATE_90))
             # Add legend
             A.paste(L, (5, 5))
 
@@ -150,7 +150,7 @@ def main():
             d.text(((A.width - w)/2, (A.height - 10)), msg, font = fnt, stroke_width = 1, stroke_fill = "black")
 
             A = A.convert("RGB")
-            A.save(os.path.join(args.out_dir, f'bia-slice{i:03}.jpg'))
+            A.save(os.path.join(args.out_dir, f'bia-slice{i:03}.jpg'), format = 'JPEG', subsampling = 0, quality = 100)
         return 0
     except Exception as e:
         print(e)